97a635a6f075426d7881e2431def27cba7109c23
Solo <creeper467@mail.ru>
2024-10-15 21:33:36 +0300

    add shad/cn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <div>
       <p className="text-red-500"> aboba</p>
+      <Button> click me</Button>
     </div>
   );
 }
